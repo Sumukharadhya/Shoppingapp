ca79e991c412890923db3ebe9a92147dfe7084da
Sumukh <sumukharadhyakj07@gmail.com>
2022-09-20 23:09:59 +0530

    Initial Commit

---
+++ Shoppingapp/ShoppingappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingappApp.swift
+//  Shoppingapp
+//
+//  Created by Sumukh Kj on 20/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
